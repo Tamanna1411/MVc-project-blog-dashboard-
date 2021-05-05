e38afda57ae3db29ca623fd1a9cbd17a2fa0d965
Tamanna <59220508+Tamanna1411@users.noreply.github.com>
2021-05-05 11:10:33 +0600

    Add files via upload

---
+++ BlogPostModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BLOGW.Models
+{
+    public class BlogPostModel
+    {
+        // General properties  
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+       public List<string> Tags { get; set; }
+
+
+        public string id { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+        public string role { get; set; }
+        public string address { get; set; }
+        public string birthdate { get; set; }
+        public string name { get; set; }
+
+
+        // Time based properties  
+        public DateTime CreateTime { get; set; }
+
+        // Other properties and settings may include UserID, RoleID etc.  
+    }
+
+    // The class to manage the data-sources  
+    public class PostManager
+    {
+        // Define the members  
+        private static string PostsFile = HttpContext.Current.Server.MapPath("~/App_Data/Posts.json");
+        private static List<BlogPostModel> posts = new List<BlogPostModel>();
+
+        // The CRUD functions  
+        public static void Create(string postJson)
+        {
+            var obj = JsonConvert.DeserializeObject<BlogPostModel>(postJson);
+
+            if (posts.Count > 0)
+            {
+                posts = (from post in posts
+                         orderby post.CreateTime
+                         select post).ToList();
+                obj.ID = posts.Last().ID + 1;
+            }
+            else
+            {
+                obj.ID = 1;
+            }
+            posts.Add(obj);
+            save();
+        }
+         public static List<BlogPostModel> Read()
+        {
+            // Check if the file exists.  
+            if (!File.Exists(PostsFile))
+            {
+                File.Create(PostsFile).Close();
+                File.WriteAllText(PostsFile, "[]"); // Create the file if it doesn't exist.  
+            }
+            posts = JsonConvert.DeserializeObject<List<BlogPostModel>>(File.ReadAllText(PostsFile));
+            return posts;
+        }
+
+        public static void Update(int id, string postJson)
+        {
+            Delete(id);
+            Create(postJson);
+            save();
+        }
+
+        public static void Delete(int id)
+        {
+            posts.Remove(posts.Find(x => x.ID == id));
+            save();
+        }
+
+        // Output function  
+        private static void save()
+        {
+            File.WriteAllText(PostsFile, JsonConvert.SerializeObject(posts));
+        }
+    }
+}  
--- HomeController.cs
+++ HomeController.cs
@@ -4,27 +4,158 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using BLOGW.Models;
+using Newtonsoft.Json;
 namespace BLOGW.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            var blogs = PostManager.Read();
+            if (blogs == null)
+            {
+                ViewBag.Empty = true;
+                return View();
+            }
+            else
+            {
+                // Just for sorting.  
+                blogs = (from blog in blogs
+                         orderby blog.CreateTime descending
+                         select blog).ToList();
+
+                ViewBag.Empty = false;
+                return View("index", blogs);
+            }
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            if (Request.HttpMethod == "POST")
+            {
+                // Post request method  
+                var Id = Request.Form["Id"].ToString();
+                var Name = Request.Form["Name"].ToString();
+                var Email = Request.Form["Email"].ToString();
+                var password = Request.Form["password"].ToString();
+                var Role = Request.Form["Role"].ToString();
+                var Address = Request.Form["Address"].ToString();
+                var Birthdate = Request.Form["Birthdate"].ToString();
 
-            return View();
+                // Save content  
+                var post = new BlogPostModel { id = Id, CreateTime = DateTime.Now, name = Name, email= Email, Password= password, role= Role, address= Address, birthdate= Birthdate };
+                PostManager.Create(JsonConvert.SerializeObject(post));
+
+                // Redirect  
+                Response.Redirect("~/Home/Index");
+            }
+            return View("About");
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            var blogs = PostManager.Read();
+            if (blogs == null)
+            {
+                ViewBag.Empty = true;
+                return View();
+            }
+            else
+            {
+                // Just for sorting.  
+                blogs = (from blog in blogs
+                         orderby blog.CreateTime descending
+                         select blog).ToList();
 
-            return View();
+                ViewBag.Empty = false;
+                return View("Contact", blogs);
+            }
         }
+        [Route("Home/read/{id}")]
+    
+        // GET: Blog
+        
+        public ActionResult Read(int id)
+        {
+            // Read one single blog  
+            var blogs = PostManager.Read();
+            BlogPostModel post = null;
+
+            if (blogs != null && blogs.Count > 0)
+            {
+                post = blogs.Find(x => x.ID == id);
+            }
+
+            if (post == null)
+            {
+                ViewBag.PostFound = false;
+                return View();
+            }
+            else
+            {
+                ViewBag.PostFound = true;
+                return View("ViewPage", post);
+            }
+        }
+        public ActionResult Create()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                // Post request method  
+                var title = Request.Form["title"].ToString();
+                var tags = Request.Form["tags"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var content = Request.Form["content"].ToString();
+
+                // Save content  
+                var post = new BlogPostModel { Title = title, CreateTime = DateTime.Now, Content = content, Tags = tags.ToList() };
+                PostManager.Create(JsonConvert.SerializeObject(post));
+
+                // Redirect  
+                Response.Redirect("~/Home/Contact");
+            }
+            return View("Createpost");
+        }
+        [Route("Home/edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                // Post request method  
+                var title = Request.Form["title"].ToString();
+                var tags = Request.Form["tags"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var content = Request.Form["content"].ToString();
+
+                // Save content  
+                var post = new BlogPostModel { Title = title, CreateTime = DateTime.Now, Content = content, Tags = tags.ToList() };
+                PostManager.Update(id, JsonConvert.SerializeObject(post));
+
+                // Redirect  
+                Response.Redirect("~/Home/Contact");
+            }
+            else
+            {
+                // Find the required post.  
+                var post = PostManager.Read().Find(x => x.ID == id);
+
+                if (post != null)
+                {
+                    // Set the values  
+                    ViewBag.Found = true;
+                    ViewBag.PostTitle = post.Title;
+                    ViewBag.Tags = post.Tags;
+                    ViewBag.Content = post.Content;
+                }
+                else
+                {
+                    ViewBag.Found = false;
+                }
+            }
+
+            // Finally return the view.  
+            return View("Updatepage");
+        }
+
     }
+
 }
